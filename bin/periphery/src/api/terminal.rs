7cedf58ba31e90c26cf47691004dece1a0e7385b
mbecker20 <becker.maxh@gmail.com>
2025-05-03 21:17:03 -0700

    use printf again

--- bin/periphery/src/api/terminal.rs
+++ bin/periphery/src/api/terminal.rs
@@ -346,7 +346,7 @@ pub async fn execute_terminal(
   );
 
   let full_command = format!(
-    "echo '{START_OF_OUTPUT}'; {command}; rc=$? printf '\n{KOMODO_EXIT_CODE}%d\n{END_OF_OUTPUT}\n' \"$rc\"\n"
+    "printf '{START_OF_OUTPUT}\n'; {command}; rc=$? printf '\n{KOMODO_EXIT_CODE}%d\n{END_OF_OUTPUT}\n' \"$rc\"\n"
   );
 
   terminal
