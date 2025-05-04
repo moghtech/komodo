284047d676a97eac7705d0fbba85e596c70ffa1f
mbecker20 <becker.maxh@gmail.com>
2025-05-03 20:58:54 -0700

    echo

--- bin/periphery/src/api/terminal.rs
+++ bin/periphery/src/api/terminal.rs
@@ -346,7 +346,7 @@ pub async fn execute_terminal(
   );
 
   let full_command = format!(
-    "printf '{START_OF_OUTPUT}\n'; {command}; rc=$? printf '\n{KOMODO_EXIT_CODE}%d\n{END_OF_OUTPUT}\n' \"$rc\"\n"
+    "echo '{START_OF_OUTPUT}'; {command}; rc=$? printf '\n{KOMODO_EXIT_CODE}%d\n{END_OF_OUTPUT}\n' \"$rc\"\n"
   );
 
   terminal
