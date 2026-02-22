68e973762b6b558569be63a772194479bd61b094
mbecker20 <becker.maxh@gmail.com>
2026-02-22 13:27:03 -0800

    UserAddUserGroup

--- ui/src/pages/user/member-groups.tsx
+++ ui/src/pages/user/member-groups.tsx
@@ -1,14 +1,27 @@
-import { useRead } from "@/lib/hooks";
+import { useInvalidate, useRead, useWrite } from "@/lib/hooks";
 import { ICONS } from "@/theme/icons";
 import Section from "@/ui/section";
-import { Group, Text } from "@mantine/core";
-import { Link } from "react-router-dom";
+import { Box, Group, Text } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
+import UserAddUserGroup from "./add-group";
+import { notifications } from "@mantine/notifications";
+import ConfirmIcon from "@/ui/confirm-icon";
 
 export default function UserMemberGroups({ userId }: { userId: string }) {
-  const groups = useRead("ListUserGroups", {}).data?.filter((group) =>
-    group.users?.includes(userId),
+  const allGroups = useRead("ListUserGroups", {}).data;
+  const groups = allGroups?.filter((group) => group.users?.includes(userId));
+  const nav = useNavigate();
+  const inv = useInvalidate();
+  const { mutate: remove, isPending: removePending } = useWrite(
+    "RemoveUserFromUserGroup",
+    {
+      onSuccess: () => {
+        inv(["ListUserGroups"]);
+        notifications.show({ message: "Removed user from group" });
+      },
+    },
   );
-  if (!groups || groups.length === 0) {
+  if (!allGroups || allGroups.length === 0) {
     return null;
   }
   return (
@@ -17,30 +30,46 @@ export default function UserMemberGroups({ userId }: { userId: string }) {
       icon={<ICONS.UserGroup size="1.2rem" />}
       titleFz="h3"
       titleMb="0"
+      titleRight={
+        <Box ml="md">
+          <UserAddUserGroup userId={userId} />
+        </Box>
+      }
       withBorder
     >
-      <Group>
-        {groups.map((group) => (
-          <Group
-            key={group._id?.$oid}
-            title={`User Group - ${group.name}`}
-            renderRoot={(props) => (
-              <Link to={`/user-groups/${group._id?.$oid}`} {...props} />
-            )}
-            className="accent-hover-light bordered-heavy"
-            py="xs"
-            px="md"
-            bdrs="sm"
-            wrap="nowrap"
-            gap="xs"
-          >
-            <ICONS.UserGroup size="1rem" />
-            <Text className="hover-underline" style={{ textWrap: "nowrap" }}>
-              {group.name}
-            </Text>
-          </Group>
-        ))}
-      </Group>
+      {groups?.length ? (
+        <Group>
+          {groups?.map((group) => (
+            <Group
+              key={group._id?.$oid}
+              title={`User Group - ${group.name}`}
+              onClick={() => nav(`/user-groups/${group._id?.$oid}`)}
+              className="accent-hover-light bordered-heavy"
+              py="xs"
+              px="md"
+              bdrs="sm"
+              wrap="nowrap"
+              gap="sm"
+              style={{ cursor: "pointer" }}
+            >
+              <ICONS.UserGroup size="1rem" />
+              <Text className="hover-underline" style={{ textWrap: "nowrap" }}>
+                {group.name}
+              </Text>
+              <ConfirmIcon
+                variant="filled"
+                color="red"
+                onClick={() =>
+                  remove({ user_group: group._id?.$oid!, user: userId })
+                }
+                loading={removePending}
+              >
+                <ICONS.Remove size="1rem" />
+              </ConfirmIcon>
+            </Group>
+          ))}
+        </Group>
+      ) : null}
     </Section>
   );
 }
