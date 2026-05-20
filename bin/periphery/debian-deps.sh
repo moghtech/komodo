d812a8133bce71aa2c6df7cc26c36c24695c3115
shady <shadybradengithub@shady.icu>
2026-05-20 12:12:27 -0400

    infisical setup and install

--- bin/periphery/debian-deps.sh
+++ bin/periphery/debian-deps.sh
@@ -9,6 +9,11 @@ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
+# Infisical setup
+curl -fsSL 'https://artifacts-cli.infisical.com/setup.deb.sh' -o setup.deb.sh
+chmod +x setup.deb.sh
+./setup.deb.sh
+
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
@@ -17,7 +22,7 @@ echo \
 apt-get update
 
 # apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-apt-get install -y docker-ce-cli docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce-cli docker-buildx-plugin docker-compose-plugin infisical
 
 rm -rf /var/lib/apt/lists/*
 
