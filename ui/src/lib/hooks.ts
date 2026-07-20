c27045ade38fee0d07197b94dbf0be7f50c13215
mbecker20 <becker.maxh@gmail.com>
2026-07-20 12:58:46 -0700

    user permissioning fix search

--- ui/src/lib/hooks.ts
+++ ui/src/lib/hooks.ts
@@ -815,50 +815,6 @@ export function useDashboardPreferences() {
   };
 }
 
-export function useUserTargetPermissions(user_target: Types.UserTarget) {
-  const permissions = useRead("ListUserTargetPermissions", {
-    user_target,
-  }).data;
-  const allResources = useAllResources(undefined, 0);
-  const perms: (Types.Permission & { name: string })[] = [];
-  for (const [resource_type, resources] of Object.entries(allResources)) {
-    addUserTargetPermissions(
-      user_target,
-      permissions,
-      resource_type as UsableResource,
-      resources,
-      perms,
-    );
-  }
-  return perms;
-}
-
-function addUserTargetPermissions<I>(
-  user_target: Types.UserTarget,
-  permissions: Types.Permission[] | undefined,
-  resource_type: UsableResource,
-  resources: Types.ResourceListItem<I>[] | undefined,
-  perms: (Types.Permission & { name: string })[],
-) {
-  resources?.forEach((resource) => {
-    const perm = permissions?.find(
-      (p) =>
-        p.resource_target.type === resource_type &&
-        p.resource_target.id === resource.id,
-    );
-    if (perm) {
-      perms.push({ ...perm, name: resource.name });
-    } else {
-      perms.push({
-        user_target,
-        name: resource.name,
-        level: Types.PermissionLevel.None,
-        resource_target: { type: resource_type, id: resource.id },
-      });
-    }
-  });
-}
-
 /**
  * Checks if target operation is cancelling
  * by querying relevant Updates on the target.
